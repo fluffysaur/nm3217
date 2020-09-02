ba9878b9e2321f9b581aa69f93f2884b47e2487b
fluffysaur <tanyijia@gmail.com>
2020-09-02 16:00:10 +0800

    New Post

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -38,4 +38,19 @@
       });
   }
 
+  // Scroll Up Button
+  $.scrollUp({
+    scrollName: 'scrollUp', // Element ID
+    topDistance: '300', // Distance from top before showing element (px)
+    topSpeed: 300, // Speed back to top (ms)
+    animation: 'slide', // Fade, slide, none
+    animationInSpeed: 200, // Animation in speed (ms)
+    animationOutSpeed: 200, // Animation out speed (ms)
+    easingType: 'easeInOutExpo',
+    scrollText: '<i class="fas fa-angle-up"></i>', // Text for element
+    activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+    scrollSpeed: 900,
+    animation: 'slide'
+  });
+
 })(jQuery); // End of use strict
